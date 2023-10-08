66955bb0428d8f5597a9e8af15f294f0ac7171c0
gaurab-keen <gaurab.kumar@fosteringlinux.com>
2023-10-08 17:01:27 +0530

    Your second app

--- src/index.js
+++ src/index.js
@@ -1,58 +1,58 @@
-// import React, { useEffect } from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// function Main() {
-//   useEffect(() => {
-//     // Your useEffect logic here
-//     console.log('useEffect in Main component sdfasdfgsdgsd');
-
-//     // Clean-up function (optional)
-//     window.addEventListener('message', receiveMessage);
-
-//     // Function to handle received messages
-//     function receiveMessage(event) {
-//      console.log('11111111111111');
-//         // Access the data sent from the parent window
-//         const receivedData = event.data;
-//         // Do something with the received data
-//         //console.log('Received data in the iframe:', receivedData);
-//         console.log('4444444477788888', JSON.stringify(receivedData));
-        
-         
-//     }
-
-//     // Clean up the event listener when the component unmounts
-//     return () => {
-//         window.removeEventListener('message', receiveMessage);
-//     }; 
-//   }, []); // Empty dependency array for componentDidMount behavior
-
-//   return (
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>
-//   );
-// }
-
-// root.render(<Main />);
-
-
-
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function Main() {
+  useEffect(() => {
+    // Your useEffect logic here
+    console.log('useEffect in Main component sdfasdfgsdgsd');
+
+    // Clean-up function (optional)
+    window.addEventListener('message', receiveMessage);
+
+    // Function to handle received messages
+    function receiveMessage(event) {
+     console.log('11111111111111');
+        // Access the data sent from the parent window
+        const receivedData = event.data;
+        // Do something with the received data
+        //console.log('Received data in the iframe:', receivedData);
+        console.log('4444444477788888', JSON.stringify(receivedData));
+        
+         
+    }
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+        window.removeEventListener('message', receiveMessage);
+    }; 
+  }, []); // Empty dependency array for componentDidMount behavior
+
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+root.render(<Main />);
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 
 
